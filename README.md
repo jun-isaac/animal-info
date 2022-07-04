88f35a8c58e1fdee375a66a3baa81c2ce5bda015
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-07-04 11:52:19 +0900

    수정

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 1. 메인, 서브 페이지 구성
 2. 모바일 텝 메뉴 구현
 3. 이미지요소 + 텍스트 정보의 동물 썹네일 구성
-4. 서버에서 데이터 불러오기(자바스크립트)  
+4. 서버에서 데이터 불러오기(자바스크립트)
+    - DB URL - https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.json
     - 서버(db) ---> 클라이언트
     - 자바스크립트 fetch API 
     - 자바스크립트로 동물정보 출력
--- js/app.js
+++ js/app.js
@@ -1,13 +1,58 @@
-fetch('https://raw.githubusercontent.com/csslick/animal-info/master/animal.json')
+// 서버에 데이터 요청(request)
+fetch('https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.json')
     .then(function (res) {
         return res.json(); // JSON 객체 변환
     })
-    .then(function (data) {
+    .then(function (obj) {
         // 최종 데이터 출력(object)
-        console.log(data);
-        let name = data[0].name;
-        let category = data[0].category;
-        console.log(`name = ${name}`);
-        console.log(`category = ${category}`);
+        showProducts(obj);
 
+        /*
+        for(let i = 0; i < obj.length; i++){
+            let html =`
+            <div class="col">
+                <img src="${obj[i].imgUrl}" alt="dog01">
+                <p class="name">${obj[i].name}</p>
+            </div>
+            `
+            $('.row').append(html);
+        }
+        */
+
+        // forEach() 반복문
+        function showProducts(obj) {
+            // URL query 파라미터(매개변수)
+            const query = location.search;
+            console.log(query);
+            // ? URL query문을 object(변수)로 변경
+            const params = new URLSearchParams(query).get('category');
+            console.log(params);
+
+
+            // 동물 데이터 출력
+            obj.forEach(function (animal) {
+                // console.log(animal.name);
+                // 카테고리 구분 dog | cat | bird
+                // 요청한 params와 동물 카테고리명이 일치하는 데이터만 출력
+                if (params == animal.category) {
+                    let html = `
+                    <div class="col">
+                        <img src="${animal.imgUrl}" alt="dog01">
+                        <p class="name">${animal.name}</p>
+                    </div>
+                `
+                    $('.row').append(html);
+                }
+            });
+        }
     });
+
+
+/* 파라미터를 전달하여 요청하기
+    홈페이지주소?name=홍길동
+    매개변수(URL parameter) name = '홍길동'
+
+    index.html -> index.html?category=dog -> 개 보여주시오
+    cat.html -> cat.html?category=cat -> 고양이 보여주시오
+    bird.html -> bird.html?category=bird -> 새 보여주시오
+*/
